ee0f9d1ae72d935d9439db8bc60fa71f550ceabd
sanjanaramgarhia <sanjanaramgarhia@gmail.com>
2026-06-04 00:30:34 +0530

    Mixins Generics and Viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('employees', views.EmployeeViewSet, basename='employee')
 
 urlpatterns = [
     path('students/', views.studentsView),
     path('students/<int:pk>/', views.studentDetailView),
+    path('', include(router.urls)),
+
+    # path('employees/', views.Employees.as_view()),
+    # path('employees/<int:pk>/', views.EmployeeDetail.as_view()),
 ]
